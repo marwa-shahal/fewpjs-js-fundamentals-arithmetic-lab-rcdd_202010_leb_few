578592c1ce330ad0107896a8bbb8f2fbec90915e
Marwa Sh <marwa.al.shahal@gmail.com>
2020-11-26 15:49:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+//multiply
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+multiply;
+
+
+//random greater than 0
+let random = Math.random();
+
+
+//mod set to an equation whose remainder= 4 
+
+let mod;
+mod=2%3;
+
+
+
+// max=20;
+let max;
+max=Math.max(1, 2, 4, 20, 16);
